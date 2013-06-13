425e3448dcf835ff819531ef0d78ed124468cffb
Jonathan <jonathan@Kytelabs.com>
2013-06-13 07:20:17 -0400

    Hooking up JSON to Ramons sexy new code.

---
+++ public/js/invoice.js
@@ -0,0 +1,24 @@
+$(document).ready(function (e) {
+
+    console.log($.url().param('form'));
+    var formID = $.url().param('form');
+
+    $.getJSON('http://digidocgov.herokuapp.com/forms/' + formID + '.json', function (response) {
+
+        $(".content-master").html("");
+
+        $(".content-master").append("<h1>Llenar Formulario</h1><form id='formData'><p style='margin-top:2em; line-height:1.5em;'><strong>Titulo: </strong>" + response.title + "<br/><strong>Descripcion: </strong>" + response.description + "<br/><strong>Agencia: </strong>" + response.agency + "<br/><strong>Tipo de Contrato: </strong>" + response.documentType + "</p><hr />");
+
+        var datafields = response.datafields;
+
+        var stringToAppend = "<div class='control-group dynamicInputs dynamicForms'>";
+        for (var i = 0; i < datafields.length; i++) {
+            //<input name='datafields[][name]' class='input-xxlarge' type='text' placeholder='Ej. Tipo de Contrato, Jefe de Agencia, etc.' /> <select name='datafields[][fieldType]'><option value='text' selected>Text Field</option><option value='text'>Large Field</option><option value='text'>Radio Button</option></select>
+            stringToAppend += "<input name='' class='input-xxlarge' type='hidden' value='" + datafields[i].name + "' />";
+            stringToAppend += "<br />";
+        }
+        stringToAppend += "</div></form>";
+        $(".content-master").append(stringToAppend);
+    });
+
+});
