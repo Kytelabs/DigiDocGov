11e7bf4087c733c290e37704c6e4f01d3fb9f84e
Jonathan <jonathan@Kytelabs.com>
2013-06-13 03:01:42 -0400

    Hooking up JSON to Ramons sexy new code.

--- public/js/main.js
+++ public/js/main.js
@@ -45,97 +45,6 @@ $(document).ready(function (e) {
         }
 
     }
-
-
-    $.getJSON('http://leanpad.herokuapp.com/experiments/' + curexperiment + '.json?callback=?', function (response) {
-
-        //$("#body").html(JSON.stringify(response));
-
-        $(".experiment-info").html("<div class='row-fluid'><div class='span10'><div class='row-fluid'><div class='span10'><h1>" + response.title + "</h1></div></div><div class='row-fluid'><div class='span4'><strong>Start:</strong> " + response.startTime + "</div><div class='span6'><strong>Total Interviews:</strong> " + response.totalInterviews + "</div></div><div class='row-fluid'><div class='span4'><strong>End:</strong> " + response.endTime + "</div><div class='span6'><strong>Customer Segement:</strong> " + response.custsegTitle + "</div></div><div class='row-fluid'><div class='span10'><p>" + response.description + "</p></div></div></div><div class='span2'><div class='jumbo-experiment'><div class='jumbo-number'>" + experimentNumber + "</div></div></div></div>");
-
-        var infoDiv = $("#info");
-		
-        var demographic = response.demofields;
-        var pains = response.painhypos;
-        var gains = response.gainhypos;
-		
-		var stringToAppend;
-				
-        infoDiv.append("<h2>Demographics</h2>");
-		
-		stringToAppend = "<ol>";
-		for(var i = 0; i < demographic.length; i++)
-		{
-			stringToAppend += "<li>" + demographic[i].title + ": " + demographic[i].typeof  + "</li>";
-        }
-		stringToAppend += "</ol>";
-		infoDiv.append(stringToAppend);
-		
-        $("#info").append("<h2>Pains</h2>")
-		
-		stringToAppend = "<ol>";
-		for(var i = 0; i < pains.length; i++)
-		{
-            stringToAppend += "<li>" + pains[i].title + "</li>";
-        }
-		stringToAppend += "</ol>";
-		infoDiv.append(stringToAppend);
-		
-        $("#info").append("<h2>Gains</h2>");
-		
-		stringToAppend = "<ol>";
-		for(var i = 0; i < gains.length; i++)
-		{
-            stringToAppend += "<li>" + gains[i].title + "</li>";
-        }
-		stringToAppend += "</ol>";
-		infoDiv.append(stringToAppend);
-
-        done(1);
-    });
-	
-	$.getJSON('http://leanpad.herokuapp.com/experiments/' + curexperiment + '/interviews.json?callback=?', function (response) {
-		
-		var peopleDiv = $("#people");
-		
-		if(response.length == 0)
-		{
-			peopleDiv.prepend("<p>No interviews yet.</p>");	
-		}
-		else
-		{
-			peopleDiv.prepend("<table id='peopleTable' class='table table-striped table-hover'><thead><tr><th>#</th><th>Name</th><th>Email</th><th>Phone</th><th>Date</th><th>Delete</th><th></th></tr></thead><tbody></tbody></table>");
-			
-			peopleDiv = peopleDiv.find("tbody");
-			
-			for(var i = 0; i < response.length; i++)
-			{
-				peopleDiv.append("<tr data-toggle='tooltip' rel='tooltip' data-container='body'><td><a href='http://leanpad.herokuapp.com/interviews/" + response[i]._id + ".json' class='table-anchor'>" + (i+1) + "</a></td><td>" + response[i].name + "</td><td>" + response[i].email + "</td><td>" + response[i].phone + "</td><td>" + response[i].interviewDate + "</td><td><a class='deletePerson' href='http://leanpad.herokuapp.com/interviews/" + response[i]._id + ".json'>Delete</a></td><td><input type='checkbox'></td></tr>");
-			}
-		}
-        done(2);
-	});	
-	
-	
-	$.getJSON('http://leanpad.herokuapp.com/experiments/' + curexperiment + '/stats.json?callback=?', function (response)
-	{
-
-		var statsDiv = $("#stats");
-		var demographic = response.demographics;
-		
-        var pains = response.pains;
-        var gains = response.gains;
-
-		statsDiv.append("<h2>Demographics</h2>");
-		createChartsDemo(statsDiv, demographic)
-		statsDiv.append("<h2>Pains</h2>");
-		createChartsPG(statsDiv, pains)
-		statsDiv.append("<h2>Gains</h2>");
-		createChartsPG(statsDiv, gains)
-
-        done(3);
-		
-	});
 	
 	$("#statTab").click(function(event)
 	{
@@ -200,13 +109,10 @@ $(document).ready(function (e) {
         window.location.replace("/interviews/new");
     });
 
-	$('#people').on( 'click', 'tr', function (event) 
+	$('#info').on( 'click', 'tr', function (event) 
 	{
 		if (event.target.nodeName != "A" & event.target.nodeName != "BUTTON" & event.target.nodeName != "INPUT") {
 		    window.location.href = $(this).find("a:first").attr("href");
-		}
-		else if (event.target.nodeName == "A") {
-		    window.location.href = $(this).find(".deletePerson").attr("href");
 		}
 		else {
 		    // Do nothing
