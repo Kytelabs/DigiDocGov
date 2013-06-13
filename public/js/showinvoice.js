9fca28703198aed67a47e49ba9fa2f137880b23d
Jonathan <jonathan@Kytelabs.com>
2013-06-13 10:45:43 -0400

    Fixing a few rather nasty bugs.

--- public/js/showinvoice.js
+++ public/js/showinvoice.js
@@ -11,9 +11,9 @@ $(document).ready(function (e) {
 
         var leString = "<h1>Informacion de la Solicitud</h1>";
 
-        for (var key in p) {
+        for (var key in response) {
             if (p.hasOwnProperty(key) && validKey(key)) {
-                leString += "<p style='margin-top:2em; line-height:1.5em;'><strong>" + key + ": </strong>" + p[key] + "<br/>";
+                leString += "<p style='margin-top:2em; line-height:1.5em;'><strong>" + key + ": </strong>" + response[key] + "<br/>";
             }
         }
 
